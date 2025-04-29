79be16a3a458c3694e05741a0544c44b440c5d3a
Adriano <adrianno.aguedes@gmail.com>
2025-04-29 00:55:36 -0300

    CRUD Ativos

---
+++ server/models/Ativo.js
@@ -0,0 +1,15 @@
+class Ativo {
+    constructor(id, nome, codInterno, descricao, valor, status, dataAquisicao, qtdReparos) {
+      this.id = id;
+      this.nome = nome;
+      this.codInterno = codInterno;
+      this.descricao = descricao;
+      this.valor = valor;
+      this.status = status;
+      this.dataAquisicao = dataAquisicao;
+      this.qtdReparos = qtdReparos;
+    }
+  }
+  
+  export default Ativo;
+  
